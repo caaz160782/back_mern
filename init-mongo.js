ac5a8ce9a100773fc877bca98c623b9bd21b26e1
carlos <caaz160782@gmail.com>
2024-09-25 22:04:36 -0600

    docker mongo

--- init-mongo.js
+++ init-mongo.js
@@ -1,2 +1,12 @@
-db = db.getSiblingDB('UPTASK_MERN');
-// db.createCollection('exampleCollection');  // Optional: create a sample collection
+// init-mongo.js
+db = db.getSiblingDB('UPTASK'); // Cambia a la base de datos UPTASK
+db.createCollection('exampleCollection'); // Crea una colección de ejemplo
+
+// Crea un usuario con permisos de lectura y escritura
+db.createUser({
+  user: 'uptask_admin',
+  pwd: '9UptasKadmiN0', // Cambia esto a una contraseña segura
+  roles: [
+    { role: 'readWrite', db: 'UPTASK' }
+  ]
+});
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,13 @@
 import mongoose, { Connection } from "mongoose";
 import config from "./config";
 
-const connectionMongo= `mongodb://${config.db.user}:${config.db.password}@${config.db.host}/${config.db.nameDB}`;
+const MONGO_URI= `mongodb://${config.db.user}:${config.db.password}@${config.db.host}/${config.db.nameDB}`;
 //const connectionMongo= `mongodb://${config.db.user}:${config.db.password}@${config.db.host}/`;
-console.log(connectionMongo)
+//console.log(connectionMongo)
 
 const connect = (): Promise<typeof mongoose> => {
   return new Promise((resolve, reject) => {
-    mongoose.connect(connectionMongo);
+    mongoose.connect(MONGO_URI);
 
     const db: Connection = mongoose.connection;
 
@@ -19,7 +19,6 @@ const connect = (): Promise<typeof mongoose> => {
     db.on("error", (error) => {
       //console.error("Connection failed: " + error);
       reject(error);
-
     });
   });
 };
